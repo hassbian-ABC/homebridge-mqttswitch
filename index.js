c4014ab647102f5e04e5339a87b8cf68ecfad1e8
hassbian-ABC <37896214+hassbian-ABC@users.noreply.github.com>
2018-06-13 12:44:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-// MQTT Switch Accessory plugin for HomeBridge
-//
-// Remember to add accessory to config.json. Example:
-// "accessories": [
-//     {
-//            "accessory": "mqttswitch",
-//            "name": "PUT THE NAME OF YOUR SWITCH HERE",
-//            "url": "PUT URL OF THE BROKER HERE",
-//			  "username": "PUT USERNAME OF THE BROKER HERE",
-//            "password": "PUT PASSWORD OF THE BROKER HERE"
-// 			  "caption": "PUT THE LABEL OF YOUR SWITCH HERE",
-// 			  "topics": {
-// 				"statusGet": 	"PUT THE MQTT TOPIC FOR THE GETTING THE STATUS OF YOUR SWITCH HERE",
-// 				"statusSet": 	"PUT THE MQTT TOPIC FOR THE SETTING THE STATUS OF YOUR SWITCH HERE"
-// 			  },
-//			  "onValue": "OPTIONALLY PUT THE VALUE THAT MEANS ON HERE (DEFAULT true)",
-//			  "offValue": "OPTIONALLY PUT THE VALUE THAT MEANS OFF HERE (DEFAULT false)",
-//			  "statusCmd": "OPTIONALLY PUT THE STATUS COMMAND HERE",
-//			  "integerValue": "OPTIONALLY SET THIS TRUE TO USE 1/0 AS VALUES",
-//     }
-// ],
-//
-// When you attempt to add a device, it will ask for a "PIN code".
-// The default code for all HomeBridge accessories is 031-45-154.
-
 'use strict';
 
 var Service, Characteristic;
 var mqtt = require("mqtt");
 
+
 function MqttSwitchAccessory(log, config) {
-  	this.log        	= log;
+  	this.log          	= log;
   	this.name 			= config["name"];
   	this.url 			= config["url"];
     this.publish_options = {
-    	qos: 	((config["qos"] !== undefined) ? config["qos"] : 0),
-		retain: ((config["retain"] !== undefined) ? config["retain"] : false)
+      qos: ((config["qos"] !== undefined)? config["qos"]: 0)
     };
 	this.client_Id 		= 'mqttjs_' + Math.random().toString(16).substr(2, 8);
 	this.options = {
@@ -49,7 +24,7 @@ function MqttSwitchAccessory(log, config) {
 			topic: 'WillMsg',
 			payload: 'Connection Closed abnormally..!',
 			qos: 0,
-			retain: ((config["retain"] !== undefined) ? config["retain"] : false)
+			retain: false
 		},
 	    username: config["username"],
 	    password: config["password"],
@@ -58,25 +33,37 @@ function MqttSwitchAccessory(log, config) {
 	this.caption		= config["caption"];
 	this.topicStatusGet	= config["topics"].statusGet;
 	this.topicStatusSet	= config["topics"].statusSet;
-    this.onValue 		= (config["onValue"] !== undefined) ? config["onValue"]: "true";
-    this.offValue 		= (config["offValue"] !== undefined) ? config["offValue"]: "false";
+    this.onValue = (config["onValue"] !== undefined) ? config["onValue"]: "true";
+    this.offValue = (config["offValue"] !== undefined) ? config["offValue"]: "false";
 	if (config["integerValue"]) {
-		this.onValue 	= "1";
-		this.offValue 	= "0";
+		this.onValue = "1";
+		this.offValue = "0";
 	}
-    this.statusCmd 		= config["statusCmd"];
 
-	this.switchStatus 	= false;
+	this.switchStatus = false;
 
-	this.service 		= new Service.Switch(this.name);
-  	this.service
-    	.getCharacteristic(Characteristic.On)
-    	.on('get', this.getStatus.bind(this))
-    	.on('set', this.setStatus.bind(this));
-
+	if (config.type == "lightbulb") {
+		this.service = new Service.Lightbulb(this.name);
+		this.service
+    	    .getCharacteristic(Characteristic.On)
+    	    .on('get', this.getStatus.bind(this))
+    	    .on('set', this.setStatus.bind(this));
+	} else if (config.type == "switch") {
+        this.service = new Service.Switch(this.name);
+		this.service
+    	    .getCharacteristic(Characteristic.On)
+    	    .on('get', this.getStatus.bind(this))
+    	    .on('set', this.setStatus.bind(this));
+	} else if (config.type == "outlet") {
+		this.service = new Service.Outlet(this.name);
+		this.service
+    	    .getCharacteristic(Characteristic.On)
+    	    .on('get', this.getStatus.bind(this))
+    	    .on('set', this.setStatus.bind(this));
+	}
 	// connect to MQTT broker
-	this.client 		= mqtt.connect(this.url, this.options);
-	var that 			= this;
+	this.client = mqtt.connect(this.url, this.options);
+	var that = this;
 	this.client.on('error', function () {
 		that.log('Error event on MQTT');
 	});
@@ -84,10 +71,8 @@ function MqttSwitchAccessory(log, config) {
 	this.client.on('message', function (topic, message) {
 		if (topic == that.topicStatusGet) {
 			var status = message.toString();
-            if (status == that.onValue || status == that.offValue) {
-			    that.switchStatus = (status == that.onValue) ? true : false;
-		   	    that.service.getCharacteristic(Characteristic.On).setValue(that.switchStatus, undefined, 'fromSetValue');
-            }
+			that.switchStatus = (status == that.onValue) ? true : false;
+		   	that.service.getCharacteristic(Characteristic.On).setValue(that.switchStatus, undefined, 'fromSetValue');
 		}
 	});
     this.client.subscribe(this.topicStatusGet);
@@ -101,9 +86,6 @@ module.exports = function(homebridge) {
 }
 
 MqttSwitchAccessory.prototype.getStatus = function(callback) {
-    if (this.statusCmd !== undefined) {
-    this.client.publish(this.topicStatusSet, this.statusCmd, this.publish_options);
-    }
     callback(null, this.switchStatus);
 }
 
@@ -116,5 +98,5 @@ MqttSwitchAccessory.prototype.setStatus = function(status, callback, context) {
 }
 
 MqttSwitchAccessory.prototype.getServices = function() {
-	return [this.service];
+  return [this.service];
 }
